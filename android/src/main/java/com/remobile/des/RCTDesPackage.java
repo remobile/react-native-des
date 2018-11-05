0ee822a3888e9a4877547b75da2638937237cba3
PizzaLiu <pizza@pizzaliu.com>
2018-11-05 18:49:45 +0800

    fix for new version RN

--- android/src/main/java/com/remobile/des/RCTDesPackage.java
+++ android/src/main/java/com/remobile/des/RCTDesPackage.java
@@ -20,7 +20,7 @@ public class RCTDesPackage implements ReactPackage {
         );
     }
 
-    @Override
+    //@Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
